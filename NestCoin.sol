66b7531f161ce3852c01203ed5a054be1ca088b4
frankudoags <54338168+frankudoags@users.noreply.github.com>
2022-04-05 13:45:26 +0100

    Create NestCoin.sol

---
+++ NestCoin.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.5.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+
+contract NestToken is ERC20, Ownable {
+    constructor() ERC20("NestToken", "NST") {}
+
+    //Events for the front-end
+    event SingleReward(address to, uint256 amount);
+    event BatchRewards(address [] _recipients, uint256 [] _amounts);
+
+
+    //Reward single customer
+    function SingleRewardmint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+        emit SingleReward(to, amount);
+    }
+
+    //Reward Multiple customers at once
+    //Input format for the array of addresses: ["0x1234....", "0x2345...", ...]
+    //Input format for the array of amounts to be distributed: ["100", "200", ...]
+    function BatchRewardMint(address [] memory _recipients, uint256 [] memory _amounts ) public payable onlyOwner {
+
+        for(uint i = 0; i< _recipients.length; ++i){
+             require(_recipients[i] != address(0));
+            _mint(_recipients[i], _amounts[i]);
+        }
+        emit BatchRewards(_recipients, _amounts);
+    }
+}
