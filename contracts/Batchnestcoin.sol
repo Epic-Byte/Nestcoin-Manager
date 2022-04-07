be45eb738926b77b1c6d50e526c75bd709a069bc
"Godand" <"aezeigwe2003@gmail.com">
2022-04-07 08:44:53 -0700

    Added admin role

--- contracts/Batchnestcoin.sol
+++ contracts/Batchnestcoin.sol
@@ -3,26 +3,38 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts@4.5.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.5.0/access/AccessControl.sol";
+//import "@openzeppelin/contracts@4.5.0/access/AccessControl.sol";
 
-contract NestToken is ERC20, Ownable, AccessControl {
+contract NestToken is ERC20/*,AccessControl*/ {
+
+   // States
+   // bytes32 public constant NESTCOIN_ADMIN = keccak256("NESTCOIN_ADMIN");
+
+    constructor() ERC20("NestToken", "NST") {
+       // _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+       // _setupRole(NESTCOIN_ADMIN, msg.sender);
+       //gives developer admin role
+       Roles[msg.sender]=true;
+    }
+    //map user to adminrole
+    mapping (address=>bool)private Roles;
+
+    //checkes that users admin role is true
+    modifier onlyAdmin{
+      require(Roles[msg.sender]==true, "no permission");
+      _;
+    }
+
+
     //Events for the front-end
     event SingleReward(address to, uint256 amount);
     event BatchRewards(address [] _recipients, uint256 [] _amounts);
     event singleAmount(address [] _recipients, uint256 _amount);
     event burnedToken (address _addr, uint256 _burned);
 
-    // States
-    bytes32 public constant NESTCOIN_ADMIN = keccak256("NESTCOIN_ADMIN");
-
-    constructor() ERC20("NestToken", "NST") {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(NESTCOIN_ADMIN, msg.sender);
-    }
-
+   
     //Reward single customer
-    function SingleRewardMint(address to, uint256 amount) public onlyRole(NESTCOIN_ADMIN) {
+    function SingleRewardMint(address to, uint256 amount) public onlyAdmin {
          require(to != address(0),"invalid address");
         _mint(to, amount);
         emit SingleReward(to, amount);
@@ -31,7 +43,7 @@ contract NestToken is ERC20, Ownable, AccessControl {
     //Reward Multiple customers at once different amounts
     //Input format for the array of addresses: ["0x1234....", "0x2345...", ...]
     //Input format for the array of amounts to be distributed: ["100", "200", ...]
-    function BatchRewardMint(address [] memory _recipients, uint256 [] memory _amounts ) public onlyOwner {
+    function BatchRewardMint(address [] memory _recipients, uint256 [] memory _amounts ) public onlyAdmin {
       require(_recipients.length<=200, "input exceeds minting quota");
         for(uint i = 0; i< _recipients.length; ++i){
              require(_recipients[i] != address(0));
@@ -43,7 +55,7 @@ contract NestToken is ERC20, Ownable, AccessControl {
 
     //This will reward multiple customers the same amount
     //Input format for the array of addresses: ["0x1234....", "0x2345...", ...]
-      function sameRewardMint(address[] memory _recipients, uint256 _amount)public onlyOwner
+      function sameRewardMint(address[] memory _recipients, uint256 _amount)public onlyAdmin
     {
          require(_recipients.length<=200, "input exceeds minting quota");
 
@@ -56,12 +68,26 @@ contract NestToken is ERC20, Ownable, AccessControl {
     }
 
     //this burns excess tokens from total supply
-      function destroy(uint8 amount)public onlyOwner returns(uint256 , string memory)
+      function destroy(uint8 amount)public onlyAdmin returns(uint256 , string memory)
     {
         require(balanceOf(msg.sender) >= amount,"insufficient funds");
         _burn(msg.sender,amount);
         emit burnedToken(msg.sender, amount);
         return (amount, " Nestcoin Tokens burned");
+    }
+
+    //add an admin 
+    function addAdmin(address account)public onlyAdmin returns(bool)
+    {
+      Roles[account]=true;
+      return true;
+    }
+
+    // remove an admin
+    function removeAdmin(address account)public onlyAdmin returns(bytes32)
+    {
+      Roles[account]=false;
+      return "removed";
     }
 
 }
